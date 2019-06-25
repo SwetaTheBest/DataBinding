37fe8cc3e939bd3a3bd7644325f910f8a9d2883e
sweta <swetajain04@gmail.com>
2019-06-26 01:22:36 +0530

    drafted

---
+++ app/src/main/java/com/swetajain/databinding/utils/StringUtil.java
@@ -0,0 +1,14 @@
+package com.swetajain.databinding.utils;
+
+public class StringUtil {
+
+    public static String getQuantityString(int quantity){
+        return ("Qty: " + String.valueOf(quantity));
+    }
+
+    public static String convertIntToString(int value){
+        return (String.valueOf(value));
+    }
+
+
+}
